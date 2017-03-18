f7f6294800b869e09249e456f07390af3f81cabc
saillhar <tquenivet@gmail.com>
2017-03-18 18:23:30 +0100

    ajout lib
    body-parser

--- service.js
+++ service.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
+var bodyParser = require('body-parser')
 var app = express();
 
 var connection = mysql.createPool({
@@ -10,16 +11,24 @@ var connection = mysql.createPool({
     database: 'cookingapp'
 })
 
-
-app.get('/user', function(req, resp){
-    connection.getConnection(function(error, tempCont){
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.Header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     res.Header('Access-Control-Allow-Credentials', false);
+//     next();
+// });
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/user', function (req, resp) {
+    connection.getConnection(function (error, tempCont) {
         if (!!error) {
             tempCont.release();
             console.log('Error');
         } else {
             console.log('Connected');
-            tempCont.query("SELECT * from user", function (error, rows, fields){
-                if(!!error){
+            tempCont.query("SELECT * from user", function (error, rows, fields) {
+                if (!!error) {
                     console.log('Error in the query');
                 } else {
                     resp.setHeader('Access-Control-Allow-Origin', '*');
@@ -32,15 +41,15 @@ app.get('/user', function(req, resp){
         }
     })
 });
-app.get('/recipe', function(req, resp){
-    connection.getConnection(function(error, tempCont){
+app.get('/recipe', function (req, resp) {
+    connection.getConnection(function (error, tempCont) {
         if (!!error) {
             tempCont.release();
             console.log('Error');
         } else {
             console.log('Connected');
-            tempCont.query("SELECT * from recipe", function (error, rows, fields){
-                if(!!error){
+            tempCont.query('SELECT * from recipe', function (error, rows, fields) {
+                if (!!error) {
                     console.log('Error in the query');
                 } else {
                     resp.setHeader('Access-Control-Allow-Origin', '*');
@@ -48,6 +57,36 @@ app.get('/recipe', function(req, resp){
                     resp.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
                     resp.setHeader('Access-Control-Allow-Credentials', false);
                     resp.json(rows);
+                }
+            })
+        }
+    })
+});
+app.post('/recipe/create', function (req, resp) {
+    connection.getConnection(function (error, tempCont) {
+        if (!!error) {
+            tempCont.release();
+            console.log('Error create recipe cr');
+        } else {
+            console.log('Connected cr');
+            var addRecipeQuery = 'insert into recipe (name,image_url,preparation_time,cooking_time,ingredients,steps) values ('
+                + '"' + req.body.name + '",'
+                + '"' + req.body.image + '",'
+                + '"' + req.body.preparation_time + '",'
+                + '"' + req.body.cooking_time + '",'
+                + '"' + req.body.ingredients + '",'
+                + '"' + req.body.steps + '");';
+            tempCont.query(addRecipeQuery, function (error, rows, fields) {
+                if (!!error) {
+                    console.log('Error in the query create recipe cr');
+                    console.log(addRecipeQuery);
+                } else {
+                    resp.setHeader('Access-Control-Allow-Origin', '*');
+                    resp.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+                    resp.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+                    resp.setHeader('Access-Control-Allow-Credentials', false);
+                    // resp.end();
+                    resp.send(req.body);
                 }
             })
         }
@@ -57,22 +96,32 @@ app.listen(1337);
 
 /*
 
-connection.connect(function(error) {
-    if(!!error){
-        console.log('Error');
-    }else{
-        console.log('Connected');
-    }
-})
+ full server.js code:
 
-app.get('/user', function(req, resp){
-    connection.query("select name from user", function(error, rows, fields){
-    if(!!error){
-        console.log('Error request');
-    }else{
-        console.log('succes request select');
-        console.log(rows);
-    }
-    })
-})
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    bodyParser= require('body-parser');
+
+var app = express();
+
+app.set('port', 3000);
+
+app.use(express.static(path.normalize(__dirname + '/')));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing       application/x-www-form-urlencoded
+
+app.get('/main', function(req, res) {
+    var name = 'MyNameFromServer';
+    res.send(name);
+});
+
+app.post('/view1', function(req, res) {
+    console.log(res.body.desc);
+    res.end();
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 */
